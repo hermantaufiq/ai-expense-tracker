787fb5c364f9c3f0796b14223e949d1ac76afd82
Herman <hermantaufiq12@gmail.com>
2026-06-22 00:01:53 +0700

    feat: add Dark Mode toggle (Fase 7)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,13 @@
                     });
                 });
             }
+
+            // Dark Mode Initialization
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
         </script>
     </head>
     <body class="font-sans antialiased">
